8916939c6c369a1d7a7571b7f7aa978e48a8bd45
robertotarquiquispe@gamil.com <robertotarquiestevon@gmail.com>
2024-08-15 19:03:40 -0400

    Presentacion de la segunda practica

--- Punto.java
+++ Punto.java
@@ -1,45 +1,37 @@
-
 public class Punto implements IPunto {
-
     private double x;
     private double y;
     private double z;
     private Sistema sistema;
     private String etiqueta;
-
     // metodo constructor
     public Punto() {
         this.x = y = z = 0;
         this.sistema = Sistema.PLANO;
     }
-
     public Punto(double _x, double _y) {
         this.x = _x;
         this.y = _y;
         this.sistema = Sistema.PLANO;
     }
-
     public Punto(double _x, double _y, double _z) {
         this.x = _x;
         this.y = _y;
         this.z = _z;
         this.sistema = Sistema.ESPACIO;
     }
-
     public Punto(Sistema _sistema, double _x, double _y) {
         this.x = _x;
         this.y = _y;
         this.z = 0;
         this.sistema = _sistema;
     }
-
     public Punto(Sistema _sistema, double _x, double _y, double _z) {
         this.x = _x;
         this.y = _y;
         this.z = _z;
         this.sistema = _sistema;
     }
-
     public Punto(Sistema _sistema, String _etiqueta, double _x, double _y, double _z) {
         this.x = _x;
         this.y = _y;
@@ -47,68 +39,70 @@ public class Punto implements IPunto {
         this.sistema = _sistema;
         this.etiqueta = _etiqueta;
     }
-
     @Override
     public String toString() {
         return "Punto [x=" + this.x + ", y=" + this.y + ", z=" + this.z + ", sistema=" + this.sistema + "]";
     }
-
     // metodo get para acceder a la propiedad X
     public double getX() {
         return x;
     }
-
     // metodo set para darle un valor a la propiedad X
     public void setX(double x) {
         this.x = x;
     }
-
     public double getY() {
         return y;
     }
-
     public void setY(double y) {
         this.y = y;
     }
-
     public Sistema getSistema() {
         return sistema;
     }
-
     public void setSistema(Sistema sistema) {
         this.sistema = sistema;
     }
-
     public double getZ() {
         return z;
     }
-
     public void setZ(double z) {
         this.z = z;
     }
-
     public String getEtiqueta() {
         return etiqueta;
     }
-
     public void setEtiqueta(String etiqueta) {
         this.etiqueta = etiqueta;
     }
-
-
     // implementar los metodos abstractos que permitan obtener la
     // 1. la distancia entre dos puntos
     // 2. el punto medio entre dos puntos
     // para presentar mediante git la proxima clase 12/08/2024
-
     @Override
-    public double dameDistanciaEntreOtroPunto(Punto OtroOPunto) {
-
-        return 0;
+    public double dameDistanciaEntreOtroPunto(Punto otroPunto) {
+        double distancia = 0;
+        double x1 = this.x;
+        double y1 = this.y;
+        double x2 = otroPunto.getX();
+        double y2 = otroPunto.getY();
+        System.out.println(this.toString());
+        System.out.println(otroPunto.toString());
+        if (this.sistema == Sistema.PLANO) {
+            distancia = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        } else if (this.sistema == Sistema.ESPACIO) {
+        } else {
+            System.out.println("Debe proporcionar un sistema, no se puede realizar la operacion.");
+        }
+        return distancia;
     }
 
     @Override
     public Punto damePuntoMedioEntreOtroPunto(Punto OtroPunto) {
+        double xMedio = (this.x + OtroPunto.getX()) / 2;
+        double yMedio = (this.x + OtroPunto.getY()) / 2;
+        double zMedio = (this.x + OtroPunto.getZ()) / 2;
+        Punto puntomedio = new Punto();
 
         return null;
     }
--- TestMain.java
+++ TestMain.java
@@ -2,19 +2,19 @@ public class TestMain {
     public static void main(String[] args) {
         Punto puntoRojo = new Punto();
         puntoRojo.setSistema(Sistema.PLANO);
-        puntoRojo.setEtiqueta("ROJO");
-        puntoRojo.setX(4);
-        puntoRojo.setY(1.3);
+        puntoRojo.setEtiqueta(etiqueta;"ROJO");
+        puntoRojo.setX(x;4);
+        puntoRojo.setY(y;1.3);
 
         Punto puntoVerde = new Punto();
         puntoVerde.setSistema(Sistema.PLANO);
-        puntoVerde.setEtiqueta("VERDE");
-        puntoVerde.setX(6);
-        puntoVerde.setY(2.7);
+        puntoVerde.setEtiqueta(etiqueta;"VERDE");
+        puntoVerde.setX(x;6);
+        puntoVerde.setY(y;2.7);
 
         // codigo que debe de utilizar para probar la implementacion de sus metodos
         double distanciaEntrePuntoVerde = puntoRojo.dameDistanciaEntreOtroPunto(puntoVerde);
-        System.out.println(distanciaEntrePuntoVerde);
+        System.out.println("distancia = " + distanciaEntrePuntoVerde);
 
         Punto puntoMedioEntrePuntoVerde = puntoRojo.damePuntoMedioEntreOtroPunto(puntoVerde);
         System.out.println(puntoMedioEntrePuntoVerde.toString());
